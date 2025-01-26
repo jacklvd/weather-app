1ef52cc34bb1eeefd7d9bd05b87d1a15aa3e890e
jacklvd <vodanglong3103@gmail.com>
2025-01-26 16:22:32 -0500

    Initial Commit

---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Jack Vo on 1/26/25.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
