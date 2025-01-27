6de4cc0248a8acd59f96f79f7a24ac1d3602ee2b
jacklvd <vodanglong3103@gmail.com>
2025-01-26 22:00:04 -0500

    Finished the app

---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Jack Vo on 1/26/25.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView().progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    LoadingView()
+}
---
+++ WeatherApp/Views/WelcomeView.swift
@@ -0,0 +1,35 @@
+//
+//  WelcomeView.swift
+//  WeatherApp
+//
+//  Created by Jack Vo on 1/26/25.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    var body: some View {
+        VStack {
+            VStack(spacing: 20) {
+                Text("Welcome to the Weather App").bold().font(.title)
+                Text("Please share your location to get the weather in your area").padding()
+            }
+            .multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation) {
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
